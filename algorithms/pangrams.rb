1d7978b88f81dca7042ae2e454701c56c7fe777f
jin <jin@crypt.sg>
2014-11-08 16:51:56 +0800

    Refactor Pangram solution

--- algorithms/pangrams.rb
+++ algorithms/pangrams.rb
@@ -1,10 +1,13 @@
 # https://www.hackerrank.com/challenges/pangrams/
 
-str = gets.strip.downcase.split(//)
-letters = ('a'..'z').to_a
-
-str.each do |x|
-  letters.delete(x)
+def isPangram(s)
+  letters = ('a'..'z').to_a
+  s.each do |x|
+    letters.delete(x)
+    return 'pangram' if letters.empty?
+  end
+  'not pangram'
 end
 
-puts letters.empty? ? 'pangram' : 'not pangram'
+str = gets.strip.downcase.split(//)
+puts isPangram(str)
