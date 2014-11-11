663413976ea03150ccf6a088c032352c2048a918
jin <jin@crypt.sg>
2014-11-11 20:30:03 +0800

    Added solution for anagrams and palindrom index question

---
+++ algorithms/anagrams.rb
@@ -0,0 +1,24 @@
+# https://www.hackerrank.com/challenges/anagram/
+
+# Problem: break down each line of input into two, and find out the number of characters to change in one of the two strings in order to make one an anagram of the other.
+
+gets
+ARGF.each do |line|
+  line.strip!
+  if line.length.odd?
+    puts -1
+  else
+    # splitting
+    x, y = line[0..(line.length / 2) - 1].split(//), line[line.length / 2..line.length - 1].split(//)
+
+    h = {}
+    count = 0
+
+    # build hash with a counter from elements of the first half
+    x.each { |c| h[c] = h[c].nil? ? 1 : h[c] + 1 }
+
+    # decrement counter accordingly with the elements of the second half
+    y.each { |c| (h[c].nil? || h[c] == 0) ? count += 1 : (h[c] -= 1 || next) }
+    puts count
+  end
+end
---
+++ algorithms/palindrome_index.rb
@@ -0,0 +1,20 @@
+def get_offending_index(str)
+  stack = str[0..str.length / 2 - 1].split(//)
+  to_check = if str.length.odd?
+               str[(str.length / 2) + 1..-1].reverse
+             else
+               str[(str.length / 2)..-1].reverse
+             end
+  
+  while !stack.empty?
+    x = stack.pop
+    y = to_check.pop
+    next if x == y
+  end
+
+
+  -1
+end
+
+str = gets.strip.downcase
+puts get_offending_index str
