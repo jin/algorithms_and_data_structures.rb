0dc00d2f4ea4ce152abb7b34e01d0a4095da7f43
jin <jin@crypt.sg>
2014-12-08 00:51:51 +0800

    Refactored is_fibo's solution to remove side effects from the method. No global variables.

--- algorithms/is_fibonacci.rb
+++ algorithms/is_fibonacci.rb
@@ -1,16 +1,17 @@
-def is_fib? num
-  loop do
-    return true if $fibs.has_key? num
-    $first, $second = $second, ($first + $second)
-    $fibs[$second] = true
-    return true if num == $second
-    return false if $second > num
-  end
+def is_fib? num, fibs, first, second
+  return [true, fibs, first, second] if fibs.has_key? num
+  return [false, fibs, first, second] if second > num
+
+  first, second = second, (first + second)
+  fibs[second] = true
+  is_fib? num, fibs, first, second
 end
 
-$first, $second = 0, 1
-$fibs = {0 => true, 1 => true}
-gets.strip.to_i.times do 
+first, second = 0, 1
+fibs = { 0 => true, 1 => true }
+
+gets.strip.to_i.times do
   num = gets.strip.to_i
-  is_fib?(num) ? puts("IsFibo"): puts("IsNotFibo")
+  is_fib, fibs, first, second = is_fib? num, fibs, first, second
+  puts (is_fib ? "IsFibo" : "IsNotFibo")
 end
