d27d96fb05aa46b774ec3bbb81d774a63d33c625
jin <jin@crypt.sg>
2014-12-20 17:25:25 +0800

    Rewrote BF method to be iterative. Use 'if not visited[v]' instead of 'unless visited[v]' for expressiveness

--- algorithms/undirected_graph_traversal.rb
+++ algorithms/undirected_graph_traversal.rb
@@ -1,5 +1,5 @@
 # Undirected graph implemented with an adjacency list
-# 0 is root vertice 
+# 0 is root vertice
 # Vertice key is the index itself. e.g. vertice 2 = 2
 
 class UndirectedGraph
@@ -28,37 +28,42 @@ class UndirectedGraph
     @adjacency_list.flatten.count / 2
   end
 
-  def depth_first_traversal(vertice = 0, stack = [], visited = [], result = [])
-    unless visited[vertice]
+  # Recursive style
+  def depth_first_traversal(stack = [0], visited = [], result = [])
+    vertice = stack.pop
+
+    if not visited[vertice]
       result.push(vertice) # Add current vertice to the traversal result
       visited[vertice] = true # Mark current vertice as visited
       stack.push(*neighbors(vertice).reject { |v| visited[v] }) # Push unvisited neighbors onto the stack
     end
 
     # Return the result if stack is empty, else recursively continue with popped element
-    stack.empty? ? result : depth_first_traversal(stack.pop, stack, visited, result)
+    stack.empty? ? result : depth_first_traversal(stack, visited, result)
   end
 
-  def breadth_first_traversal(vertice = 0, queue = [], visited = [], result = [])
-    unless visited[vertice]
-      result.push(vertice)# Add current vertice to the traversal result
-      visited[vertice] = true  # Mark current vertice as visited
-      queue.push(*neighbors(vertice).reject { |v| visited[v] }) # Push unvisited neighbors onto the queue
-    end 
-
-    # Return the result if queue is empty, else recursively continue with dequeued element
-    queue.empty? ? result : breadth_first_traversal(queue.shift, queue, visited, result)
+  # Iterative style
+  def breadth_first_traversal(queue = [0], visited = [], result = [])
+    until queue.empty?
+      vertice = queue.shift
+      if not visited[vertice]
+        result.push(vertice)# Add current vertice to the traversal result
+        visited[vertice] = true  # Mark current vertice as visited
+        queue.push(*neighbors(vertice).reject { |v| visited[v] }) # Push unvisited neighbors onto the queue
+      end
+    end
+    result
   end
 
 end
 
 graph = UndirectedGraph.new
-graph # => #<UndirectedGraph:0x007f839a1ecd30 @adjacency_list=[[]]>
+graph # => #<UndirectedGraph:0x007fc73c1d0c30 @adjacency_list=[[]]>
 
 edges = [ [0, 8], [8, 7], [7, 6], [5, 6], [2, 5], [2, 4], [1, 4], [1, 3], [0, 1], [5, 8], [6, 1] ]
 edges.each { |edge| graph.add_edge edge[0], edge[1] }
 
-graph # => #<UndirectedGraph:0x007f839a1ecd30 @adjacency_list=[[8, 1], [4, 3, 0, 6], [5, 4], [1], [2, 1], [6, 2, 8], [7, 5, 1], [8, 6], [0, 7, 5]]>
+graph # => #<UndirectedGraph:0x007fc73c1d0c30 @adjacency_list=[[8, 1], [4, 3, 0, 6], [5, 4], [1], [2, 1], [6, 2, 8], [7, 5, 1], [8, 6], [0, 7, 5]]>
 graph.vertice_count # => 9
 graph.edge_count # => 11
 graph.depth_first_traversal # => [0, 1, 6, 5, 8, 7, 2, 4, 3]
