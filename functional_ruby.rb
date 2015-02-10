a2bf79ad8c5d71c07c8977f885bb1e50f05893cb
Jingwen Chen <jin@crypt.sg>
2015-02-10 12:44:57 +0800

    Added fizzbuzz, removed eval inputs in functional_ruby

--- functional_ruby.rb
+++ functional_ruby.rb
@@ -43,76 +43,33 @@ mul_two_func = -> (x) { x * 2 }
 
 # Testing
 
-length [] # => 0
-length [1, 2, 3] # => 3
+length []
+length [1, 2, 3]
 
-length_func.call([]) # => 0
-length_func.call([1, 2, 3]) # => 3
+length_func.call([]) 
+length_func.call([1, 2, 3])
 
-map([]) # => []
-map([1, 2]) # => [1, 2]
-map([1, 2, 3], Proc.new { |x| x.to_i * x.to_i } ) # => [1, 4, 9]
-map([1, 2, "foo"], Proc.new { |x| x.to_i * x.to_i } ) # => [1, 4, 0]
+map([])
+map([1, 2])
+map([1, 2, 3], Proc.new { |x| x.to_i * x.to_i } )
+map([1, 2, "foo"], Proc.new { |x| x.to_i * x.to_i } )
 
-add_one_func.call(2) # => 3
-add_one_func.call(add_one_func.call(2)) # => 4
+add_one_func.call(2)
+add_one_func.call(add_one_func.call(2))
 add_two_func = compose_func.call(add_one_func, add_one_func)
-add_two_func.call(2) # => 4
+add_two_func.call(2)
 add_one_then_mul_two_func = compose_func.call(mul_two_func, add_one_func)
-add_one_then_mul_two_func.call(5) # => 12
+add_one_then_mul_two_func.call(5)
 
-map_func.call([1, 2, 3], Proc.new { |x| x.to_i * x.to_i }) # => [1, 4, 9]
-map_func.call([1, 2, "foo"], Proc.new { |x| x.to_i * x.to_i }) # => [1, 4, 0]
-map_func.call([1, 2, 3], add_one_then_mul_two_func) # => [4, 6, 8]
+map_func.call([1, 2, 3], Proc.new { |x| x.to_i * x.to_i })
+map_func.call([1, 2, "foo"], Proc.new { |x| x.to_i * x.to_i })
+map_func.call([1, 2, 3], add_one_then_mul_two_func)
 
-filter_func.call([], -> (x) { x } ) # => []
-filter_func.call([1, 2, 3], -> (x) { x.odd? }) # => [1, 3]
+filter_func.call([], -> (x) { x } )
+filter_func.call([1, 2, 3], -> (x) { x.odd? })
 
-take_func.call([], 1) # => []
-take_func.call([1, 2, 3], 0) # => []
-take_func.call([1, 2, 3], 2) # => [1, 2]
+take_func.call([], 1)
+take_func.call([1, 2, 3], 0)
+take_func.call([1, 2, 3], 2)
 
 p take_func.call(map_func.call(filter_func.call((1..3000).to_a, -> (x) { x.odd? } ), -> (x) { x * x }), 42)
-# => [1,
-#     9,
-#     25,
-#     49,
-#     81,
-#     121,
-#     169,
-#     225,
-#     289,
-#     361,
-#     441,
-#     529,
-#     625,
-#     729,
-#     841,
-#     961,
-#     1089,
-#     1225,
-#     1369,
-#     1521,
-#     1681,
-#     1849,
-#     2025,
-#     2209,
-#     2401,
-#     2601,
-#     2809,
-#     3025,
-#     3249,
-#     3481,
-#     3721,
-#     3969,
-#     4225,
-#     4489,
-#     4761,
-#     5041,
-#     5329,
-#     5625,
-#     5929,
-#     6241,
-#     6561,
-#     6889]
-# >> [1, 9, 25, 49, 81, 121, 169, 225, 289, 361, 441, 529, 625, 729, 841, 961, 1089, 1225, 1369, 1521, 1681, 1849, 2025, 2209, 2401, 2601, 2809, 3025, 3249, 3481, 3721, 3969, 4225, 4489, 4761, 5041, 5329, 5625, 5929, 6241, 6561, 6889]
--- quicksort.rb
+++ quicksort.rb
@@ -5,4 +5,4 @@ def quicksort(arr)
   quicksort(left) + [pivot] + quicksort(right)
 end
 
-p quicksort [6, 5, 8, 2, 5, 2, 9, 8, 1, 0, 3, -6]
+quicksort [6, 5, 8, 2, 5, 2, 9, 8, 1, 0, 3, -6] # => [-6, 0, 1, 2, 2, 3, 5, 5, 6, 8, 8, 9]
