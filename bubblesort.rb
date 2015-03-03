aa1b2a88dbc765e84add0c9c17537b14626d1414
Jingwen Chen <jin@crypt.sg>
2015-03-03 15:28:31 +0800

    implemented bubblesort

---
+++ bubblesort.rb
@@ -0,0 +1,19 @@
+def bubble_sort ary
+  ary = ary.clone
+  sorted = false 
+
+  until sorted
+    sorted = true
+    0.upto(ary.length - 2) do |idx|
+      if ary[idx] > ary[idx + 1]
+        ary[idx], ary[idx + 1] = ary[idx + 1], ary[idx]
+        sorted = false
+      end
+    end
+  end
+
+  ary
+end
+
+ary = [*1..100].to_a.shuffle
+bubble_sort ary # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
